ce2ecb1b30a5ff08f6d9d85c744a408171cdd124
EC2 Default User <ec2-user@ip-172-31-4-118.us-east-2.compute.internal>
2018-10-03 02:33:08 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,56 +11,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.all_ratings
-  
-   #is there a new session
-  if session[:sort] != true
-    session[:sort] = true
-    session[:selected_ratings] = @all_ratings
+    @movies = Movie.all
   end
-  
-  
-  if params[:sort] != nil
-     session[:sort] = params[:sort]
-  elsif session[:sort] != nil
-    redirect_to :rating => params[:rating], :sort => session[:sort] and return
-  end
-  
-  
-  
-    #If the params not nil, set the session to those params
-    if params[:rating] != nil
-     session[:selected_ratings] = params[:ratings]
-    elsif params[:rating] == nil and session[:selected_ratings] == nil
-     session[:selected_ratings] = @all_ratings
-     
-  
-    if params[:rating] != nil
-      session[:selected_ratings] = params[:rating]
-    elsif params[:rating] == nil and session[:selected_ratings] == nil
-      session[:selected_ratings] = @all_ratings
-      flash.keep
-      redirect_to movies_path :sort => @sort_choice, :ratings => @selected_ratings
-    end
-  
-    @selected_ratings = session[:selected_ratings]
-    @sort = session[:sort]
-  
-  
-    case @sort 
-    when 'title'
-      @movies = Movie.where(:rating => @selected_ratings.keys).order(:title)
-      @title_head = 'hilite'
-    when 'release_date' 
-      @movies = Movie.where(:rating => @selected_ratings.keys).order(:release_date)
-      @release_head = 'hilite'
-    else
-      @movies = Movie.all
-    end
-    
-    end 
-  end
- 
+
   def new
     # default: render 'new' template
   end
@@ -88,5 +41,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-end
 
+end
